6e7fe559d27240c5664aae917310fdf1545f008e
v4leng <valem.g_13@hotmail.com>
2026-04-21 23:12:26 -0300

    Error login

--- next-app/my-app/src/lib/axios.js
+++ next-app/my-app/src/lib/axios.js
@@ -4,6 +4,8 @@ const axios = Axios.create({
     baseURL: '/api-proxy',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
     },
     withCredentials: true,
     withXSRFToken: true
