e992baa79d7dbd711bc69f0490979f48364a86ba
v4leng <valem.g_13@hotmail.com>
2026-04-21 21:55:09 -0300

    Error 419

--- next-app/my-app/next.config.mjs
+++ next-app/my-app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api-proxy/:path*',
+                destination: 'https://railway.app*',
+            },
+        ]
+    },
+}
 
 export default nextConfig;
