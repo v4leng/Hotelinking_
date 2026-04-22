6c2fd0705949fdf0713c9cd08a2033453a0e8a16
v4leng <valem.g_13@hotmail.com>
2026-04-21 23:23:59 -0300

    Error routes

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\CodeController;
 use App\Http\Controllers\OfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -14,6 +20,30 @@ Route::get('/list_codes', [CodeController::class, 'list']);
 Route::post('/code-create', [CodeController::class, 'promotion']);
 Route::put('/codes/{code}', [CodeController::class, 'redeem']);
 Route::get('/codes_verify/{codeId}', [CodeController::class, 'getCodeRedeemedStatus']);
+Route::post('/register', [RegisteredUserController::class, 'store'])
+                ->middleware('guest')
+                ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest')
+                ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.store');
+                
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware(['auth', 'throttle:6,1'])
+                ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->middleware('auth')
+                ->name('logout');
+
 
 
 
