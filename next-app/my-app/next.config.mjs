0208fef6c52b97dfa0ef6421db3433169b9888bc
v4leng <valem.g_13@hotmail.com>
2026-04-21 22:25:07 -0300

    Error

--- next-app/my-app/next.config.mjs
+++ next-app/my-app/next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api-proxy/:path*',
-                destination: 'https://hotelinking-production.up.railway.app*',
-            },
-        ]
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: 'https://hotelinking-production.up.railway.app/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig;
